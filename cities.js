b7f33f26ccc6c5f82767057d0c5e609fa72b0fa8
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-01-12 09:58:47 +0100

    Orhan's lösung

--- cities.js
+++ cities.js
@@ -1,1 +1,16 @@
+const cities = [
+  "New York",
+  "San Francisco",
+  "Los Angeles",
+  "Chicago",
+  "Las Vegas",
+  "Miami",
+  "Washington",
+  "Alabama",
+];
 
+function addCitis(name) {
+  document.write("<p class='cities'>" + name + "</p>");
+}
+
+names.map(addCitis);
