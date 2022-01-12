21a39fa9a6a861f0d58fd31199ce837146327019
Hayat Alkheder <hayat.alkheder@gmail.com>
2022-01-12 10:06:39 +0100

    Cities

--- cities.js
+++ cities.js
@@ -1,1 +1,7 @@
+const cities = ["New York", "San Francisco", "Los Angeles", "Chicago", "Las Vegas", "Miami", "Washington", "Alabama"];
+
+function addElement (city) {
+  document.write("<p class='cities'>" + city + "</p>");
+}
+cities.map(addElement);
 
