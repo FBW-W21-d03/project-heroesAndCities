5ce2d4d3aea234fb7221f1cdffac556d215e201f
slavablack <veaceslav.ciornii@magenta.de>
2022-01-12 10:07:36 +0100

    JS writedatei

--- cities.js
+++ cities.js
@@ -1,1 +1,8 @@
+const cityinAmerica =["New York", "San Francisco", "Los Angeles", "Chicago", "Las Vegas", "Miami", "Washington", "Alabama"];
 
+cityinAmerica.sort();
+function cityinHtml (city) {
+
+    document.write("<p class= city>" + city + "</p>");
+}
+cityinAmerica.map(cityinHtml);
