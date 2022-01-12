ccc28d979014f007e89171e1756bfcf1d9f0e595
kawanaza <kawanbabani@gmail.com>
2022-01-12 10:03:53 +0100

    done

--- cities.js
+++ cities.js
@@ -13,6 +13,17 @@ function addElem(cities) {
   document.write("<p class='cities'>" + cities + "</p>");
 }
 city.map(addElem);
+document.getElementsByTagName("h2")[0].style.color="red";
+document.getElementsByTagName("h2")[1].style.color="red";
 
-document.getElementsByClassName("cities")[0].style.color = "red";
-document.getElementsByClassName("cities")[1].style.color = "red";
+
+document.getElementsByTagName("body")[0].style.backgroundColor = "#A4C6F2"
+document.getElementsByClassName("cities")[0].style.boxShadow = "10px 20px 30px blue";
+document.getElementsByClassName("cities")[1].style.boxShadow = "10px 20px 30px blue";
+document.getElementsByClassName("cities")[2].style.boxShadow = "10px 20px 30px blue";
+document.getElementsByClassName("cities")[3].style.boxShadow = "10px 20px 30px blue";
+document.getElementsByClassName("cities")[4].style.boxShadow = "10px 20px 30px blue";
+
+document.getElementsByClassName("cities")[5].style.boxShadow = "10px 20px 30px blue";
+document.getElementsByClassName("cities")[6].style.boxShadow = "10px 20px 30px blue";
+document.getElementsByClassName("cities")[7].style.boxShadow = "10px 20px 30px blue";
