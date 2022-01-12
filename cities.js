69453d9cb40ba0d1752aecb8b9d14a5fd5588ea0
kawanaza <kawanbabani@gmail.com>
2022-01-12 11:56:14 +0100

    new update

--- cities.js
+++ cities.js
@@ -13,6 +13,7 @@ function addElem(cities) {
   document.write("<p class='cities'>" + cities + "</p>");
 }
 city.map(addElem);
+
 document.getElementsByTagName("h2")[0].style.color = "red";
 document.getElementsByTagName("h2")[1].style.color = "red";
 
@@ -40,11 +41,45 @@ document.getElementsByClassName("cities")[7].style.boxShadow =
 const myEdit = document.getElementsByClassName("cities");
 const btnCitys = document.getElementById("btnCity");
 
-btnCitys.addEventlistener("click", () => {
-  console.log("hallo");
-  document.getElementsByClassName("cities")[4].style.boxShadow =
-    "10px 20px 30px red";
-  //btnCitys.innerHTML = "ccc";
+function addEvent(){
+ 
+  document.getElementsByClassName("cities")[0].style.boxShadow =
+  "10px 20px 30px red";
+document.getElementsByClassName("cities")[1].style.boxShadow =
+  "10px 20px 30px red";
+document.getElementsByClassName("cities")[2].style.boxShadow =
+  "10px 20px 30px red";
+document.getElementsByClassName("cities")[3].style.boxShadow =
+  "10px 20px 30px red";
+document.getElementsByClassName("cities")[4].style.boxShadow =
+  "10px 20px 30px red";
+
+document.getElementsByClassName("cities")[5].style.boxShadow =
+  "10px 20px 30px red";
+document.getElementsByClassName("cities")[6].style.boxShadow =
+  "10px 20px 30px red";
+document.getElementsByClassName("cities")[7].style.boxShadow =
+  "10px 20px 30px red";
+
+ 
+}
+
+
+
+
+
+
+
+
+/* btnCitys.addEventlistener("click", () => {
+  
+ const cit7= document.getElementsByClassName("cities")[7].style.boxShadow =
+  "10px 20px 30px red";
+  cit7.style.backgroundColor="red";
+
 });
+ */
 
 ////////
+
+
