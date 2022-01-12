3433e498cc7c042f36e5fca272c04c58c228cd58
slavablack <veaceslav.ciornii@magenta.de>
2022-01-12 11:59:50 +0100

    City  map img

--- cities.js
+++ cities.js
@@ -13,6 +13,30 @@ function cklick(city){
  }
 if (city=="New York") {
     document.write(`<img src="./new-york.png" alt="">` );
+}
+
+if (city=="San Francisco") {
+    document.write(`<img src="./san-francisco.png" alt="">` );
+} 
+if (city=="Los Angeles") {
+    document.write(`<img src="./los-angeles.png" alt="">` );
+} 
+
+if (city=="Chicago") {
+    document.write(`<img src="./chicago.png" alt="">` );
+} 
+if (city=="Las Vegas") {
+    document.write(`<img src="./las-vegas.png" alt="">` );
+} 
+if (city=="Miami") {
+    document.write(`<img src="./miami.png" alt="">` );
+} 
+if (city=="Washington") {
+    document.write(`<img src="./waschington.jpeg" alt="">` );
+} 
+
+if (city=="Alabama") {
+    document.write(`<img src="./alabama.png" alt="">` );
 } 
 
 }
