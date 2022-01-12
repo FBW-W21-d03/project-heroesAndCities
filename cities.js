545842dd1180348f9d72bdda1fcd10f3f83c0974
Mak513 <osei.makangu@hotmail.com>
2022-01-12 09:56:21 +0100

    osei

--- cities.js
+++ cities.js
@@ -1,1 +1,15 @@
+const cities= [
+  "New York",
+  "San Francisco",
+  "Los Angeles",
+  "Chicago",
+  "Las Vegas",
+  "Miami",
+  "Washington",
+  "Alabama",
+];
 
+function addElement(city) {
+  document.write("<p class='city'>" + city + "</p>");
+}
+names.map(addElement);
