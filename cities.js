01281e407566ff6f5b93c328ef42cb0599bfab84
ShiinyFlakes <mago_elm@hotmail.com>
2022-01-12 12:45:11 +0100

    Lösung

--- cities.js
+++ cities.js
@@ -1,1 +1,6 @@
+const cities = ["New York", "San Francisco", "Los Angeles", "Chicago", "Las Vegas", "Miami", "Washington", "Alabama"];
 
+function addElement (stadt) {
+  document.write("<p class='cities'>" + stadt + "</p>");
+}
+cities.map(addElement);
--- heroes.js
+++ heroes.js
@@ -1,6 +1,6 @@
 const names = ["John", "Bob", "Chuck", "Sam", "Pete", "Sarah", "Ana", "Henry"];
 
 function addElement (name) {
-  document.write("<p class='heroes'>" + name + "</p>");
+  document.write("<p class='cities'>" + name + "</p>");
 }
 names.map(addElement);
