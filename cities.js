7babc61ccc8c5a1449b1daa55cd3f5602fd305b8
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2022-01-12 11:04:55 +0100

    Lösung von Christian mit CSS Grid

--- cities.js
+++ cities.js
@@ -1,1 +1,28 @@
+const cities = [
+  "New York",
+  "San Francisco",
+  "Los Angeles",
+  "Chicago",
+  "Las Vegas",
+  "Miami",
+  "Washington ",
+  "Alabama",
+];
 
+function addCities(city) {
+  // Original-Aufgabe
+  // document.write("<li>" + city + "</li>");
+
+  // Bonus-Aufgabe
+  document.write(`<li onclick="writeCity('${city}')">${city}</li>`);
+}
+
+document.write('<ul class="cities">');
+cities.map(addCities);
+document.write("</ul>");
+
+// Bonus-Aufgabe
+
+function writeCity(city) {
+  console.log(city);
+}
