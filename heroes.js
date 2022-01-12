1f4dca8b78134eaadbf5650db08d87c9871ab97c
slavablack <veaceslav.ciornii@magenta.de>
2022-01-12 11:25:05 +0100

    JS writedatei

--- heroes.js
+++ heroes.js
@@ -1,6 +1,6 @@
 const names = ["John", "Bob", "Chuck", "Sam", "Pete", "Sarah", "Ana", "Henry"];
 
 function addElement (name) {
-  document.write("<p class='heroes'>" + name + "</p>");
+  document.write("<button class='heroes'>" + name + "</button>");
 }
 names.map(addElement);
