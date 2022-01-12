b0d994770922bad631cde51723f03d4c96b622f0
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2022-01-12 10:06:09 +0100

    Meine Lösung

--- cities.js
+++ cities.js
@@ -1,1 +1,6 @@
+const cities = ["Berlin", "München", "Hamburg", "Frankfurt", "Leipzig", "Dresden", "Dortmünd", "Köln"];
 
+function addElement (city) {
+  document.write("<p class='cities'>" + city + "</p>");
+}
+cities.map(addElement);
