e521007ac9664b85616b6a26c544eab726792c3a
kawanaza <kawanbabani@gmail.com>
2022-01-12 09:46:54 +0100

    Done

--- cities.js
+++ cities.js
@@ -1,1 +1,18 @@
+const city = [
+  "New York",
+  "San Francisco",
+  "Los Angeles",
+  "Chicago",
+  "Las Vegas",
+  "Miami",
+  "Washington",
+  "Alabama",
+];
 
+function addElem(cities) {
+  document.write("<p class='cities'>" + cities + "</p>");
+}
+city.map(addElem);
+
+document.getElementsByClassName("cities")[0].style.color = "red";
+document.getElementsByClassName("cities")[1].style.color = "red";
