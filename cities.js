c7de83c95b6631c271bd38b4b51d99eae8d60a65
Vens <v.kolev@alfatrex.com>
2022-01-12 09:56:28 +0100

    vens

--- cities.js
+++ cities.js
@@ -1,1 +1,14 @@
+const cities = ["New York",
+"San Francisco",
+"Los Angeles",
+"Chicago",
+"Las Vegas",
+"Miami",
+"Washington",
+ "Alabama"];
+
+ function addElementCities (cities) {
+  document.write("<p class='cities'>" + cities + "</p>");
+}
+cities.map(addElementCities);
 
